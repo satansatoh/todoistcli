4825f32b6614e5e26f04f4a81df218d605ecf4af
satansatoh <wataru_310@hotmail.com>
2018-05-29 23:22:49 +0900

    プロジェクト ファイルを追加します。

---
+++ TodoistConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoistConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
